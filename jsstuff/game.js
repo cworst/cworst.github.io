e9774bd274cc7eeed0649ccd5925b113ff09d295
Lan-Madeleine Do <madsfemaledeer@gmail.com>
2025-05-14 11:46:10 -0400

    here ben you baddie

--- jsstuff/game.js
+++ jsstuff/game.js
@@ -1,6 +1,12 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+let x = 0;
+let y = 0;
+let dx = 5;
+let dy = 1;
+let score = 0;
+let gameRunning = true;
 
 function drawRect(x,y) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -13,43 +19,86 @@ function sand(x,y) {
     ctx.fillRect(x,y,400,400);
 }
 
+const Trap = {
+        //key:value pair
+        x : 100,
+        y : 200,
+        speed: 3
+};
 
-function Trap(x,y) {
+const keys = {};
+
+function drawTrap(){
 
     ctx.beginPath();
-    ctx.moveTo(x+110, y+70);
-    ctx.lineTo(x+150,y+70);
-    ctx.lineTo(x+145,y+100);
-    ctx.lineTo(x+115,y+100);
-    ctx.lineTo(x+110,y+70);
+    ctx.moveTo(Trap.x, Trap.y);
+    ctx.lineTo(Trap.x+40,Trap.y);
+    ctx.lineTo(Trap.x+35,Trap.y+30);
+    ctx.lineTo(Trap.x+5,Trap.y+30);
+    ctx.lineTo(Trap.x,Trap.y);
     ctx.stroke();
     ctx.fillStyle = 'SteelBlue';
     ctx.fill();
 }
 
-function trash(x,y) {
+function moveTrap(){
+	console.log ("moving")
+        if(keys['ArrowDown'] &&
+                Trap.y < 373){
+                Trap.y += Trap.speed;
+        }
+                if(keys['ArrowUp'] &&
+                Trap.y > 0){
+                Trap.y -= Trap.speed;
+        }
+                if(keys['ArrowLeft'] &&
+                Trap.x > 0){
+                Trap.x -= Trap.speed;
+        }
+                if(keys['ArrowRight'] &&
+                Trap.x < 363){
+                Trap.x += Trap.speed;
+        }
 
+}
+
+const trash = {
+        x : 200,
+        y : 200,
+        speed: 3
+};
+
+function drawtrash() {
     ctx.beginPath();
-    ctx.moveTo(x+110, y+70);
-    ctx.lineTo(x+150,y+70);
-    ctx.lineTo(x+145,y+100);
-    ctx.lineTo(x+115,y+100);
-    ctx.lineTo(x+110,y+70);
+    ctx.moveTo(trash.x, trash.y);
+    ctx.lineTo(trash.x+40,trash.y);
+    ctx.lineTo(trash.x+35,trash.y+30);
+    ctx.lineTo(trash.x+5,trash.y+30);
+    ctx.lineTo(trash.x,trash.y);
     ctx.stroke();
     ctx.fillStyle = 'Red';
     ctx.fill();
 }
 
-function trap(){
-    ctx.strokeStyle = "black";
-    ctx.strokeWidth = 5;
-    ctx.beginPath();
-    ctx.moveTo(20,20);
-    ctx.lineTo(100,20);
-    ctx.lineTo(175,100);
-    ctx.lineTo(20,100);
-    ctx.lineTo(20,20);
-    ctx.stroke();
+function movetrash(){
+        console.log ("moving")
+        if(keys['s'] &&
+                trash.y < 373){
+                trash.y += trash.speed;
+        }
+                if(keys['w'] &&
+                trash.y > 0){
+                trash.y -= trash.speed;
+        }
+                if(keys['a'] &&
+                trash.x > 0){
+                trash.x -= trash.speed;
+        }
+                if(keys['d'] &&
+                trash.x < 363){
+                trash.x += trash.speed;
+        }
+
 }
 
 function circ(x,y){
@@ -142,20 +191,105 @@ function gar(x,y){
     hol(x+35,y+35);
 }
 
+function drawScore(){
+            ctx.font = "30px Arial";
+            ctx.fillText(score, 10, 30);
+}
 
-drawRect(6,100);
-sand(0,0);
-//trap();
-//circ(50,40);
-bubs(50,40);
-Trap(2,100);
-trash(180,120);
-wave(40,10);
-//bub(200,200);
-//curve(300,300);
-//lon(300,300,3);
-shell(70,160,2.9);
-shell(140,140,2.9);
-//rect(100,180);
-//hol(300,300);
-gar(300,130);
+function animate() {
+    drawRect(6,100);
+    sand(0,0);
+    wave(40,10);
+    shell(70,160,2.9);
+    shell(140,140,2.9);
+    gar(300,130);
+    drawTrap();
+    moveTrap();
+    drawtrash();
+    movetrash();
+    checkCollision();
+    checkColl();
+    drawScore();
+    if(gameRunning){
+    if(score > 1000){
+        gameRunning = false;
+ }
+        requestAnimationFrame(animate);
+}
+}
+
+ function checkCollision(){
+     let wave_min_x = x;
+	 let wave_min_y = y;
+	 let wave_max_x = x+400;
+	 let wave_max_y = y+50; 
+     let Trap_min_x = Trap.x - 20;
+     let Trap_min_y = Trap.y - 20;
+     let Trap_max_x = Trap.x + 20;
+     let Trap_max_y = Trap.y + 20;
+ 
+     if( Trap_max_y > wave_min_y &&
+         Trap_min_y < wave_max_y &&
+         Trap_max_x > wave_min_x && 
+         Trap_min_x < wave_max_x){
+            gameRunning = false;
+    }
+
+      let trash_min_x = trash.x - 20;
+      let trash_min_y = trash.y - 20;
+      let trash_max_x = trash.x + 20;
+      let trash_max_y = trash.y + 20;
+  
+      if(wave_max_y > trash_min_y && wave_min_y < trash_max_y && wave_max_x > trash_min_x && wave_min_x < trash_max_x){
+          gameRunning = false;
+      }
+
+}
+
+function checkColl(){
+     let shell_min_x = x-20;
+     let shell_min_y = y-20;
+     let shell_max_x = x+20;
+     let shell_max_y = y+20; 
+     let Trap_min_x = Trap.x - 20;
+     let Trap_min_y = Trap.y - 20;
+     let Trap_max_x = Trap.x + 20;
+     let Trap_max_y = Trap.y + 20;
+ 
+     if( Trap_max_y > shell_min_y &&
+         Trap_min_y < shell_max_y &&
+         Trap_max_x > shell_min_x && 
+         Trap_min_x < shell_max_x){
+            score++;
+    }
+
+      let trash_min_x = trash.x - 20;
+      let trash_min_y = trash.y - 20;
+      let trash_max_x = trash.x + 20;
+      let trash_max_y = trash.y + 20;
+  
+      if(shell_max_y > trash_min_y &&
+         shell_min_y < trash_max_y && 
+         shell_max_x > trash_min_x && 
+         shell_min_x < trash_max_x){
+          score++;
+      }
+
+}
+
+
+function handleKeyPress(e){
+        //console.log(e.key);
+        e.preventDefault();
+        keys[e.key] = true;
+}
+
+document.addEventListener('keydown', handleKeyPress);
+document.addEventListener('keyup', (e) => {
+        //console.log(e.key + "up");
+	e.preventDefault();
+        keys[e.key] = false;
+});
+
+
+animate();

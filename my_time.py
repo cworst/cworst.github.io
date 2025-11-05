fd4bdfef73c99c4cebc8c57b242a4dbb478e0ca5
cworst <madsfemaledeer@gmail.com>
2025-11-05 12:17:34 -0500

    first try on my_time

---
+++ my_time.py
@@ -0,0 +1,7 @@
+class Time:
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+    return (str(hours) + “:” + str(minutes) + “:” + str(seconds))
+

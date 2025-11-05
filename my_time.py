b0283d71e07ab4c0a4c048e8a1d561cdaf3c4626
cworst <madsfemaledeer@gmail.com>
2025-11-05 12:34:11 -0500

    idk how to add a second zero to the seconds

--- my_time.py
+++ my_time.py
@@ -3,5 +3,9 @@ class Time:
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
-    return (str(hours) + “:” + str(minutes) + “:” + str(seconds))
+
+    def __str__(self):
+        if len(self.seconds) == 1:
+            "0" + str(self.seconds) = self.seconds
+        return (str(self.hours) + ":" + str(self.minutes) + ":" + str(self.seconds))
 
